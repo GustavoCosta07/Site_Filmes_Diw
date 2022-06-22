170b25d766d44901e65c671e36e0f47fc54b8ae2
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-21 22:34:23 -0300

    mudanças na nav

--- js/index.js
+++ js/index.js
@@ -7,27 +7,26 @@ const searchMovieUrl = baseUrl + '/search/multi?api_key=' + apiKey + '&language=
 const searchGenresUrl = baseUrl + '/genre/movie/list?api_key=' + apiKey + '&language=pt-BR'
 let paginaAnterior = null
 let proximaPagina = null
-
+let pesquisar = null
+let barraDePesquisa = null
 
 
 window.onload = () => {
 
-    paginaAnterior = document.querySelector('#prevPage') 
+    paginaAnterior = document.querySelector('#prevPage')
     paginaAnterior.disabled = true
-    proximaPagina =  document.querySelector('#proxPage')
+    proximaPagina = document.querySelector('#proxPage')
     proximaPagina.disabled = false
     getGenres()
     getMovies()
-
-    const pesquisar = document.querySelector('#pesquisar')
+    barraDePesquisa = document.getElementById('barraDePesquisa')
+    pesquisar = document.querySelector('#pesquisar')
     pesquisar.addEventListener("click", searchMovies);
-
-  
 }
 
 const getGenres = () => {
     fetch(searchGenresUrl)
-       
+
         .then(dados => dados.json())
         .then(dados => {
             genres = dados.genres
@@ -38,13 +37,12 @@ const getGenres = () => {
 const getMovies = (page = 1) => {
     const url = moviesPopularUrl + '&page=' + page
     fetch(url)
-        
         .then(dados => dados.json())
         .then(dados => {
             montarHtml(dados)
+            proximaPagina.disabled = false
+            barraDePesquisa.value = '';
         }).catch(err => console.log(err))
-    
-        proximaPagina.disabled = false
 }
 
 const montarHtml = (response) => {
@@ -53,12 +51,12 @@ const montarHtml = (response) => {
 
     let contador = 0;
     let divRow = null;
-    
+
     const filmes = response.results
-    
+
 
     for (const filme of filmes) {
-       
+
 
         const controle = filme.title ? 'movie' : 'tv';
 
@@ -111,23 +109,23 @@ const searchMovies = () => {
     const query = barraDePesquisa.value
     const url = searchMovieUrl + '&query=' + query
     fetch(url)
-        
+
         .then(data => data.json())
         .then(response => {
             montarHtml(response)
         }).catch(err => console.log(err))
 
-        paginaAnterior.disabled = true
-        proximaPagina.disabled = true
+    paginaAnterior.disabled = true
+    proximaPagina.disabled = true
 
 }
 
-const nextPage = () => { 
+const nextPage = () => {
     page++
     if (page != 1) {
-        
+
         paginaAnterior.disabled = false
-        
+
     }
     getMovies(page)
 }
@@ -136,12 +134,12 @@ const previousPage = () => {
     if (page > 1) {
         page--
         paginaAnterior.disabled = false
-    } 
-    
-    if (page === 1){
+    }
+
+    if (page === 1) {
         paginaAnterior.disabled = true
     }
-    
+
     getMovies(page)
 }
 
